9097f05a1e33e6d6b0d1345cff8beac64b382403
svyatoslavkk <sv.krivonos1@gmail.com>
2023-10-10 15:57:59 +0300

    first version done

--- src/Components/AfterSubscribe.tsx
+++ src/Components/AfterSubscribe.tsx
@@ -7,7 +7,7 @@ export default function AfterSubscribe ({ onDismiss }: any) {
             <div className="after-text">
                 <img src={imageSuccess} className="image-success" alt="Success Image" />
                 <h1 className="after-title">Thanks for subscribing!</h1>
-                <p>A confirmation email has been sent to <span style={{fontWeight: 700}}>ash@loremcompany.com</span>. Please open it and click the button inside to confirm your subscription</p>
+                <p className="after-message">A confirmation email has been sent to <span style={{fontWeight: 700}}>ash@loremcompany.com</span>. Please open it and click the button inside to confirm your subscription</p>
             </div>
             <button type="button" className="subscribe-button after" onClick={onDismiss}>Dismiss message</button>
         </div>
--- src/Components/MainPage.tsx
+++ src/Components/MainPage.tsx
@@ -5,7 +5,7 @@ import iconList from '../icon-list.svg';
 
 export default function MainPage ({ onSubscribe }: any) {
     const [email, setEmail] = useState("");
-    const [isValidEmail, setIsValidEmail] = useState(true);
+    const [isValidEmail, setIsValidEmail] = useState(false);
     const [isEmptyEmail, setIsEmptyEmail] = useState(false);
     const [isButtonClicked, setIsButtonClicked] = useState(false);
 
@@ -39,46 +39,57 @@ export default function MainPage ({ onSubscribe }: any) {
             setIsEmptyEmail(false);
             setIsValidEmail(validateEmail(newEmail));
         }
+        if (isValidEmail) {
+            setIsButtonClicked(false);
+        }
     };
 
     const handleSubscribeClick = () => {
         setIsButtonClicked(true);
-        if (isValidEmail && isEmptyEmail) {
+        if (!isEmptyEmail && isValidEmail) {
             onSubscribe();
         }
     };
+    
 
     return (
         <div className='container'>
             <img src={illustrationSignUpMobile} className="illustration-mobile" alt='Illustration Image' />
             <div className='content'>
-                <h1 className='title'>Stay Updated!</h1>
+                <h1 className='title'>Stay updated!</h1>
                 <p className='main-desc'>Join 60,000+ product managers receiving monthly updates on:</p>
-                {listArray.map((item: any) => (
-                <div className='list'>
-                    <img src={item.icon} className='icon-list' alt="Icon List" />
-                    <p className='list-item'>{item.description}</p>
+                <div className="list-block">
+                    {listArray.map((item: any) => (
+                    <div className='list'>
+                        <img src={item.icon} className='icon-list' alt="Icon List" />
+                        <p className='list-item'>{item.description}</p>
+                    </div>
+                    ))}
                 </div>
-                ))}
                 <div className="input-header">
                     <h4 className='email-title'>Email address</h4>
                     {isButtonClicked ? (
-                            <p className="error-message">Valid email required</p>
-                        ) : null
-                    }
+                        <p className={`error-message ${isEmptyEmail || !isValidEmail ? "invalid" : isEmptyEmail
+                        ? "empty"
+                        : ""}`}>Valid email required</p>
+                        ) : null}
                 </div>
                 <input 
-                    className={`email-input ${isEmptyEmail ? "empty" : ""} ${
-                        isValidEmail ? "" : "invalid"
-                      }`} 
+                    className={`email-input ${
+                        isButtonClicked && (isEmptyEmail || !isValidEmail)
+                            ? "invalid"
+                            : isEmptyEmail
+                                ? "empty"
+                                : ""
+                    }`}
                     placeholder='email@company.com' 
                     value={email}
                     onChange={handleEmailChange}
                 />
                 <button 
-                type="button" 
-                className='subscribe-button'
-                onClick={handleSubscribeClick}
+                    type="button" 
+                    className='subscribe-button'
+                    onClick={handleSubscribeClick}
                 >
                     Subscribe to monthly newsletter
                 </button>
